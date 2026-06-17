2c222db480b698c152e11afbde39e0ec2ce8debe
佂煊 <1798643961@qq.com>
2026-06-17 22:44:24 +0800

    Start mixin works

---
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/MixinCrashReport.java
@@ -0,0 +1,23 @@
+package io.izzel.arclight.common.mixin.core;
+
+import net.minecraft.CrashReport;
+import net.minecraft.SystemReport;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CrashReport.class)
+public class MixinCrashReport {
+
+    @Shadow
+    @Final
+    private SystemReport systemReport;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void arclight$init(String title, Throwable t, CallbackInfo ci) {
+        this.systemReport.setDetail("CraftBukkit Information", new org.bukkit.craftbukkit.CraftCrashReport()); // CraftBukkit
+    }
+}
